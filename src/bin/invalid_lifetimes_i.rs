542e63a000262bda1388eaa7f0ce401baab20198
psatomas <psatomas@gmail.com>
2025-10-08 06:50:38 -0300

    chore: add example demonstrating lifetime error with borrowed slice

---
+++ src/bin/invalid_lifetimes_i.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let some_cities = {
+        let cities = vec![
+            String::from("London"),
+            String::from("New York"),
+            String::from("Barcelona"),
+        ];
+
+        &cities[0..2]
+    };
+}
